b34e1ed5d344a1ab06a115f5f35596de82a5fe1e
전현빈[SW Developer] <peterhyun1234@gmail.com>
2023-06-22 21:47:21 +0900

    feat: add user module

---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './user.model';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  // CREATE
+  async create(user: Partial<User>): Promise<User> {
+    return this.userModel.create(user);
+  }
+
+  // READ
+  async findAll(): Promise<User[]> {
+    return this.userModel.findAll();
+  }
+
+  async findOne(id: string): Promise<User> {
+    return this.userModel.findByPk(id);
+  }
+
+  // UPDATE
+  async update(id: string, user: Partial<User>): Promise<number> {
+    const [affectedCount] = await this.userModel.update(user, {
+      where: { id },
+    });
+    return affectedCount;
+  }
+
+  // DELETE
+  async remove(id: string): Promise<void> {
+    const user = await this.findOne(id);
+    await user.destroy();
+  }
+}
