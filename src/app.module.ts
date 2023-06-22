c21e8c37167d7504e62decf1e05cb1f6e3349970
전현빈[SW Developer] <peterhyun1234@gmail.com>
2023-06-22 21:48:45 +0900

    feat: add some modules and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
+import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.RDS_HOSTNAME,
+      port: +process.env.RDS_PORT,
+      username: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: process.env.RDS_DB_NAME,
+      autoLoadModels: true,
+      // synchronize: true,
+    }),
+    TodosModule,
+    UsersModule,
+    SchedulesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS neodohae API')
+    .setDescription('NestJS에서 neodohae를 위한 API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT || 4200);
 }
 bootstrap();
